7d20bcd2b767cbb9659fe029f59dd80583d9cdb6
Sid <sidneym@hotmail.nl>
2020-04-08 12:07:50 +0200

    JWT token return
    Added system.identity.tokens.jwt
    Added newtonsoft.json
    
    Added post method to return jwt token if username is correct.
    Added class TokenGeneration wich generates a token based upon username.
    Added UserData model.
    Added Encryption class but not implemented.

---
+++ Petible_Auth_Service/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Petible_Auth_Service.Logic;
+using Petible_Auth_Service.Models;
+
+namespace Petible_Auth_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+       
+        public AuthController(ILogger<AuthController> logger)
+        {
+           
+        }
+
+        public object TokenGen { get; private set; }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post([FromBody] JsonElement data)
+        {
+            string json = System.Text.Json.JsonSerializer.Serialize(data);
+            UserData user = JsonConvert.DeserializeObject<UserData>(json);
+            IActionResult response = Unauthorized();
+            if (user.username == "test2" && user.password == "test2")
+            {
+                string token = TokenGeneration.GenerateToken(user.username);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
---
+++ Petible_Auth_Service/Models/UserData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Petible_Auth_Service.Models
+{
+    public class UserData
+    {
+        public string username;
+        public string password;
+        public UserData(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
